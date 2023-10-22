c2e214297c805039813b9fbe4a6306c8b1a71608
picasp <abdulrozak@students.amikom.ac.id>
2023-10-22 16:37:27 +0700

    build

--- app/src/main/java/com/example/tugas5/MainActivity.java
+++ app/src/main/java/com/example/tugas5/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tugas5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new LoginFragment())
+                .commit();
+
     }
 }
